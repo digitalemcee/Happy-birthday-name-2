c4ab6c94f2046001b668ad959c9f9a6282604f1b
Maisha Cannon <digitalemcee@users.noreply.github.com>
2016-02-26 17:04:49 -0800

    Updates to HB file
    Added more verbiage to prompts.

--- Happy.birthday-name.2.py
+++ Happy.birthday-name.2.py
@@ -1,9 +1,9 @@
-name = raw_input("What's your name? \nType name: ")
-guess = raw_input("Is today your birthday? \nyes or no: ").lower()
+name = raw_input("Good afternoon. What's your name? \nType name: ")
+guess = raw_input("I have a question - Is today your birthday? \nyes or no: ").lower()
 if guess == "yes" or guess == "y": 
-    print "Happy Birthday,",name + "!"
+    print "Happy Birthday to you,",name + "!"
 elif guess == "no" or guess == "n":
-    print "Have a nice day,",name + "!"
+    print "Have a nice day anyway,",name + "!"
 else:
-  print "You didn't type yes or no! Be fair."
-raw_input("\nSee you next time.")
+  print "You didn't type yes or no! Don't be evil!"
+raw_input("\nThanks for playing. See you next time.")
